83a559a038ee09b4f9b784445fdb7b199f8b7499
Jelp200 <jorge.pena0516@gmail.com>
2025-06-07 01:06:22 -0600

    :purple_square: [MOD] PROGRAMAS

--- app/Escritorio/Form1.Designer.cs
+++ app/Escritorio/Form1.Designer.cs
@@ -1,187 +1,232 @@
-namespace SensorMonitor
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+public class Form1 : Form
 {
-    partial class Form1
+    private ComboBox comboBoxSensor;
+    private Button buttonLeer;
+    private Label labelTemp;
+    private TextBox textBoxTemp;
+    private Label labelHum;
+    private TextBox textBoxHum;
+    private RichTextBox richTextBoxDatos;
+    private Chart chartDatos;
+    private StatusStrip statusStrip1;
+    private ToolStripStatusLabel toolStripStatusLabel1;
+    private TextBox txtComando;
+    private Button btnEnviar;
+    private Panel leftPanel;
+
+    public Form1()
     {
-        private System.ComponentModel.IContainer components = null;
+        InitializeComponent();
+    }
 
-        private System.Windows.Forms.ComboBox comboBoxSensor;
-        private System.Windows.Forms.Button buttonLeer;
-        private System.Windows.Forms.Label labelTemp;
-        private System.Windows.Forms.TextBox textBoxTemp;
-        private System.Windows.Forms.Label labelHum;
-        private System.Windows.Forms.TextBox textBoxHum;
-        private System.Windows.Forms.RichTextBox richTextBoxDatos;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chartDatos;
-        private System.Windows.Forms.StatusStrip statusStrip1;
-        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
-
-        protected override void Dispose(bool disposing)
+    private void InitializeComponent()
+    {
+        ChartArea chartArea1 = new ChartArea();
+        Series series1 = new Series();
+        Series series2 = new Series();
+        leftPanel = new Panel();
+        btnEnviar = new Button();
+        txtComando = new TextBox();
+        labelHum = new Label();
+        textBoxHum = new TextBox();
+        labelTemp = new Label();
+        textBoxTemp = new TextBox();
+        buttonLeer = new Button();
+        comboBoxSensor = new ComboBox();
+        richTextBoxDatos = new RichTextBox();
+        chartDatos = new Chart();
+        statusStrip1 = new StatusStrip();
+        toolStripStatusLabel1 = new ToolStripStatusLabel();
+        leftPanel.SuspendLayout();
+        ((System.ComponentModel.ISupportInitialize)chartDatos).BeginInit();
+        statusStrip1.SuspendLayout();
+        SuspendLayout();
+        // 
+        // leftPanel
+        // 
+        // Panel izquierdo con scroll
+        leftPanel = new Panel
         {
-            if (disposing && (components != null))
+            Dock = DockStyle.Left,
+            Width = 400,
+            AutoScroll = true,
+            Padding = new Padding(10)
+        };
+
+        // Tabla para organizar en 2 columnas
+        TableLayoutPanel botonesTabla = new TableLayoutPanel
+        {
+            ColumnCount = 2,
+            AutoSize = true,
+            AutoSizeMode = AutoSizeMode.GrowAndShrink,
+            Dock = DockStyle.Top
+        };
+
+        // Fijamos ancho relativo de cada columna
+        botonesTabla.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+        botonesTabla.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+
+        // Agrega controles generales (sensor, temp, etc.)
+        botonesTabla.Controls.Add(comboBoxSensor, 0, 0);
+        botonesTabla.SetColumnSpan(comboBoxSensor, 2);
+
+        botonesTabla.Controls.Add(buttonLeer, 0, 1);
+        botonesTabla.SetColumnSpan(buttonLeer, 2);
+
+        botonesTabla.Controls.Add(labelTemp, 0, 2);
+        botonesTabla.Controls.Add(textBoxTemp, 1, 2);
+
+        botonesTabla.Controls.Add(labelHum, 0, 3);
+        botonesTabla.Controls.Add(textBoxHum, 1, 3);
+
+        // Agrega botones de comando en múltiples filas
+        int fila = 4;
+        for (int i = 0; i < 10; i++)
+        {
+            Button btn = new Button
             {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
+                Text = $"Comando {i + 1}",
+                Dock = DockStyle.Fill,
+                Height = 30,
+                Tag = $"CMD{i + 1}"
+            };
+            btn.Click += BotonComando_Click;
+            int col = i % 2;
+            if (col == 0) botonesTabla.RowCount++;
+            botonesTabla.Controls.Add(btn, col, fila + i / 2);
         }
 
-        #region Windows Form Designer generated code
+        // Comando y richtextbox
+        int siguienteFila = fila + (int)Math.Ceiling(10 / 2.0);
+        botonesTabla.Controls.Add(txtComando, 0, siguienteFila);
+        botonesTabla.Controls.Add(btnEnviar, 1, siguienteFila);
+        botonesTabla.Controls.Add(richTextBoxDatos, 0, siguienteFila + 1);
+        botonesTabla.SetColumnSpan(richTextBoxDatos, 2);
 
-        private void InitializeComponent()
+        // Agrega la tabla al panel con scroll
+        leftPanel.Controls.Add(botonesTabla);
+
+        // 
+        // btnEnviar
+        // 
+        btnEnviar.Location = new Point(0, 0);
+        btnEnviar.Name = "btnEnviar";
+        btnEnviar.Size = new Size(75, 23);
+        btnEnviar.TabIndex = 0;
+        // 
+        // txtComando
+        // 
+        txtComando.Location = new Point(0, 0);
+        txtComando.Name = "txtComando";
+        txtComando.Size = new Size(100, 23);
+        txtComando.TabIndex = 1;
+        // 
+        // labelHum
+        // 
+        labelHum.Location = new Point(0, 0);
+        labelHum.Name = "labelHum";
+        labelHum.Size = new Size(100, 23);
+        labelHum.TabIndex = 2;
+        // 
+        // textBoxHum
+        // 
+        textBoxHum.Location = new Point(0, 0);
+        textBoxHum.Name = "textBoxHum";
+        textBoxHum.Size = new Size(100, 23);
+        textBoxHum.TabIndex = 3;
+        // 
+        // labelTemp
+        // 
+        labelTemp.Location = new Point(0, 0);
+        labelTemp.Name = "labelTemp";
+        labelTemp.Size = new Size(100, 23);
+        labelTemp.TabIndex = 4;
+        // 
+        // textBoxTemp
+        // 
+        textBoxTemp.Location = new Point(0, 0);
+        textBoxTemp.Name = "textBoxTemp";
+        textBoxTemp.Size = new Size(100, 23);
+        textBoxTemp.TabIndex = 5;
+        // 
+        // buttonLeer
+        // 
+        buttonLeer.Location = new Point(0, 0);
+        buttonLeer.Name = "buttonLeer";
+        buttonLeer.Size = new Size(75, 23);
+        buttonLeer.TabIndex = 6;
+        // 
+        // comboBoxSensor
+        // 
+        comboBoxSensor.Location = new Point(0, 0);
+        comboBoxSensor.Name = "comboBoxSensor";
+        comboBoxSensor.Size = new Size(121, 23);
+        comboBoxSensor.TabIndex = 7;
+        // 
+        // richTextBoxDatos
+        // 
+        richTextBoxDatos.Location = new Point(0, 0);
+        richTextBoxDatos.Name = "richTextBoxDatos";
+        richTextBoxDatos.Size = new Size(100, 96);
+        richTextBoxDatos.TabIndex = 8;
+        richTextBoxDatos.Text = "";
+        // 
+        // chartDatos
+        // 
+        chartArea1.Name = "ChartArea1";
+        chartDatos.ChartAreas.Add(chartArea1);
+        chartDatos.Location = new Point(0, 0);
+        chartDatos.Name = "chartDatos";
+        series1.ChartArea = "ChartArea1";
+        series1.Name = "Temperatura";
+        series2.ChartArea = "ChartArea1";
+        series2.Name = "Humedad";
+        chartDatos.Series.Add(series1);
+        chartDatos.Series.Add(series2);
+        chartDatos.Size = new Size(300, 300);
+        chartDatos.TabIndex = 0;
+        // 
+        // statusStrip1
+        // 
+        statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+        statusStrip1.Location = new Point(0, 531);
+        statusStrip1.Name = "statusStrip1";
+        statusStrip1.Size = new Size(1089, 22);
+        statusStrip1.TabIndex = 2;
+        // 
+        // toolStripStatusLabel1
+        // 
+        toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+        toolStripStatusLabel1.Size = new Size(0, 17);
+        // 
+        // Form1
+        // 
+        ClientSize = new Size(1089, 553);
+        Controls.Add(chartDatos);
+        Controls.Add(leftPanel);
+        Controls.Add(statusStrip1);
+        Name = "Form1";
+        Text = "Monitor de Sensores";
+        WindowState = FormWindowState.Maximized;
+        leftPanel.ResumeLayout(false);
+        leftPanel.PerformLayout();
+        ((System.ComponentModel.ISupportInitialize)chartDatos).EndInit();
+        statusStrip1.ResumeLayout(false);
+        statusStrip1.PerformLayout();
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    private void BotonComando_Click(object sender, EventArgs e)
+    {
+        if (sender is Button btn && btn.Tag is string comando)
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            comboBoxSensor = new ComboBox();
-            buttonLeer = new Button();
-            labelTemp = new Label();
-            textBoxTemp = new TextBox();
-            labelHum = new Label();
-            textBoxHum = new TextBox();
-            richTextBoxDatos = new RichTextBox();
-            chartDatos = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            statusStrip1 = new StatusStrip();
-            toolStripStatusLabel1 = new ToolStripStatusLabel();
-            txtComando = new TextBox();
-            btnEnviar = new Button();
-            ((System.ComponentModel.ISupportInitialize)chartDatos).BeginInit();
-            statusStrip1.SuspendLayout();
-            SuspendLayout();
-            // 
-            // comboBoxSensor
-            // 
-            comboBoxSensor.FormattingEnabled = true;
-            comboBoxSensor.Location = new Point(20, 20);
-            comboBoxSensor.Name = "comboBoxSensor";
-            comboBoxSensor.Size = new Size(200, 23);
-            comboBoxSensor.TabIndex = 2;
-            // 
-            // buttonLeer
-            // 
-            buttonLeer.Location = new Point(240, 20);
-            buttonLeer.Name = "buttonLeer";
-            buttonLeer.Size = new Size(100, 23);
-            buttonLeer.TabIndex = 3;
-            buttonLeer.Text = "Leer Datos";
-            buttonLeer.UseVisualStyleBackColor = true;
-            // 
-            // labelTemp
-            // 
-            labelTemp.AutoSize = true;
-            labelTemp.Location = new Point(20, 60);
-            labelTemp.Name = "labelTemp";
-            labelTemp.Size = new Size(101, 15);
-            labelTemp.TabIndex = 4;
-            labelTemp.Text = "Temperatura (°C):";
-            // 
-            // textBoxTemp
-            // 
-            textBoxTemp.Location = new Point(140, 60);
-            textBoxTemp.Name = "textBoxTemp";
-            textBoxTemp.ReadOnly = true;
-            textBoxTemp.Size = new Size(100, 23);
-            textBoxTemp.TabIndex = 5;
-            // 
-            // labelHum
-            // 
-            labelHum.AutoSize = true;
-            labelHum.Location = new Point(20, 90);
-            labelHum.Name = "labelHum";
-            labelHum.Size = new Size(84, 15);
-            labelHum.TabIndex = 6;
-            labelHum.Text = "Humedad (%):";
-            // 
-            // textBoxHum
-            // 
-            textBoxHum.Location = new Point(140, 90);
-            textBoxHum.Name = "textBoxHum";
-            textBoxHum.ReadOnly = true;
-            textBoxHum.Size = new Size(100, 23);
-            textBoxHum.TabIndex = 7;
-            // 
-            // richTextBoxDatos
-            // 
-            richTextBoxDatos.Location = new Point(20, 130);
-            richTextBoxDatos.Name = "richTextBoxDatos";
-            richTextBoxDatos.ReadOnly = true;
-            richTextBoxDatos.Size = new Size(320, 100);
-            richTextBoxDatos.TabIndex = 8;
-            richTextBoxDatos.Text = "";
-            // 
-            // chartDatos
-            // 
-            chartArea1.Name = "ChartArea1";
-            chartDatos.ChartAreas.Add(chartArea1);
-            chartDatos.Location = new System.Drawing.Point(360, 20);
-            chartDatos.Name = "chartDatos";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Temperatura";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Humedad";
-            chartDatos.Series.Add(series1);
-            chartDatos.Series.Add(series2);
-            chartDatos.Size = new System.Drawing.Size(750, 210);
-            this.Controls.Add(chartDatos);
-            chartDatos.TabIndex = 9;
-            chartDatos.Text = "Gráfica de Sensores";
-            // 
-            // statusStrip1
-            // 
-            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
-            statusStrip1.Location = new Point(0, 281);
-            statusStrip1.Name = "statusStrip1";
-            statusStrip1.Size = new Size(1139, 22);
-            statusStrip1.TabIndex = 10;
-            // 
-            // toolStripStatusLabel1
-            // 
-            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            toolStripStatusLabel1.Size = new Size(73, 17);
-            toolStripStatusLabel1.Text = "Estado: Listo";
-            // 
-            // txtComando
-            // 
-            txtComando.Location = new Point(20, 240);
-            txtComando.MaxLength = 3;
-            txtComando.Name = "txtComando";
-            txtComando.Size = new Size(100, 23);
-            txtComando.TabIndex = 0;
-            // 
-            // btnEnviar
-            // 
-            btnEnviar.Location = new Point(140, 240);
-            btnEnviar.Name = "btnEnviar";
-            btnEnviar.Size = new Size(100, 23);
-            btnEnviar.TabIndex = 1;
-            btnEnviar.Text = "Enviar Comando";
-            btnEnviar.UseVisualStyleBackColor = true;
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1139, 303);
-            Controls.Add(txtComando);
-            Controls.Add(btnEnviar);
-            Controls.Add(comboBoxSensor);
-            Controls.Add(buttonLeer);
-            Controls.Add(labelTemp);
-            Controls.Add(textBoxTemp);
-            Controls.Add(labelHum);
-            Controls.Add(textBoxHum);
-            Controls.Add(richTextBoxDatos);
-            Controls.Add(chartDatos);
-            Controls.Add(statusStrip1);
-            Name = "Form1";
-            Text = "Monitor de Sensores";
-            ((System.ComponentModel.ISupportInitialize)chartDatos).EndInit();
-            statusStrip1.ResumeLayout(false);
-            statusStrip1.PerformLayout();
-            ResumeLayout(false);
-            PerformLayout();
+            MessageBox.Show($"Se envió el comando: {comando}");
         }
-
-        #endregion
     }
 }
